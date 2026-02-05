f230e56b9a5790f6881d893984b4584ef2408e22
Raíssa Ruana <rrbsiilva@gmail.com>
2026-02-05 18:35:07 -0300

    criação das urls

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Categoria, Cliente, Pedido, Produto, ItemPedido, VariacaoProduto
+from .forms import PedidoForm, ItemPedidoDoceFormSet, ItemPedidoSalgadoFormSet, ItemPedidoForm
+from .models import Categoria, Cliente, Pedido, Produto, ItemPedido, VariacaoProduto, ItemPedidoSalgado, ItemPedidoDoce
 
 
 def home(request):
     return render(request, 'home.html')
 
 def novo_pedido(request):
-    pedido = Pedido.objects.create()
-    return redirect('pedido_servico', pedido_id=pedido.id)
+    if request.method == 'POST':
+        form = PedidoForm(request.POST)
+        if form.is_valid():
+            pedido = form.save(commit=False)
+            pedido.status = 'rascunho'
+            pedido.save()
+            return redirect('pedido_detalhe', pedido_id=pedido.id)
+    else:
+        form = PedidoForm()
+    return render (request, 'pedido/novo_pedido.html', {'form' : form})
 
-def pedido_servico(request, pedido_id):
-    pedido = get_object_or_404(Pedido, id=pedido_id)
-    tipos = [('KIT', 'kit festa'), ('BOLO', 'bolo'), ('DOCES', 'doces'), ('SALGADOS', 'salgados'), ('CUPCAKE', 'cupcake'), ('CASEIRINHO', 'Caseirinho')]
-
-    if request.method == "POST":
-        tipo = request.POST.get('tipo')
-        pedido.servico = tipo
-        pedido.save(update_fields=['servico'])
-        return redirect('listar_produtos', pedido_id=pedido.id, tipo=tipo)
-    return render (request, 'pedido/servico.html', {'pedido':pedido, 'tipos': tipos})
 
 def listar_produtos(request, pedido_id, tipo):
     pedido =get_object_or_404(Pedido, id=pedido_id)
@@ -36,15 +35,76 @@ def escolher_variacao(request, produto_id, pedido_id):
         quantidade = int(request.POST.get('quantidade', 1))
         variacao = get_object_or_404(VariacaoProduto, id=variacao_id)
 
-        ItemPedido.objects.create(
+        item = ItemPedido.objects.create(
             pedido=pedido,
             produto=produto,
             variacao=variacao,
             quantidade=quantidade
         )
-        return  redirect('pedido_personalizado', pedido_id=pedido.id)
+
+        if produto.tipo in ['KIT', 'BOLO', 'CUPCAKE', 'CASEIRINHO']:
+            return redirect('item_pedido_editar', item_id=item.id)
+        
+        return  redirect('pedido_detalhe', pedido_id=pedido.id)
     return render (request, 'pedido/escolher_variacao.html', {
         'pedido':pedido,
         'produto': produto,
         'variacoes': variacoes
     })
+
+def pedido_detalhe(request, pedido_id):
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+    return render(request, 'pedido/detalhe.html', {'pedido': pedido})
+
+def item_pedido_editar(request, item_id):
+    item = get_object_or_404(ItemPedido, id=item_id)
+    doce_formset= None
+    salgado_formset = None
+
+    if request.method == 'POST':
+        form = ItemPedidoForm(request.POST, request.FILES, instance=item)
+
+        if item.produto.tipo == 'KIT':
+            doce_formset = ItemPedidoDoceFormSet(request.POST, instance=item)
+            salgado_formset = ItemPedidoSalgadoFormSet(request.POST, instance=item)
+
+            if form.is_valid() and doce_formset.is_valid() and salgado_formset.is_valid():
+                form.save()
+                doce_formset.save()
+                salgado_formset.save()
+                return redirect('pedido_detalhe', pedido_id=item.pedido.id)
+        else:
+            if form.is_valid():
+                form.save()
+                return redirect('pedido_detalhe', pedido_id=item.pedido.id)
+    else:
+        form = ItemPedidoForm(instance=item)
+
+        if item.produto.tipo == 'KIT':
+            doce_formset = ItemPedidoDoceFormSet(instance=item)
+            salgado_formset = ItemPedidoSalgadoFormSet(instance=item)
+
+    return render (request, 'pedido/item_editar.html', {'item':item, 'form':form, 'doce_formset':doce_formset, 'salgado_formset':salgado_formset})
+
+def item_pedido_remover(request, item_id):
+    if request.method == 'POST':
+        item = get_object_or_404(ItemPedido, id=item_id)
+        pedido_id = item.pedido.id
+
+        item.delete()
+        return redirect('pedido_detalhe', pedido_id=pedido_id)
+
+def pedido_finalizar(request, pedido_id):
+    pedido = get_object_or_404(Pedido, id=pedido_id)
+
+    if pedido.status != 'rascunho':
+        return redirect('pedido_detalhe', pedido_id=pedido.id)
+
+    if not pedido.itens.exists():
+        return redirect('pedido_detalhe', pedido_id=pedido.id)
+    
+    pedido.status = 'finalizado'
+    pedido.save()
+
+    return redirect('pedido_detalhe', pedido_id=pedido.id)
